5a379cb1bc458f0a78b1ec367bd8a0e766054668
Zubaidov <zubaidov.shohruz@gmail.com>
2023-12-07 09:13:33 +0400

    pip list show

--- init.sql
+++
@@ -1,7 +0,0 @@
--- init.sql
-CREATE TABLE IF NOT EXISTS quiz_questions (
-    id SERIAL PRIMARY KEY,
-    question_text TEXT NOT NULL,
-    answer_text TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
